f41b6908629e489345ccb636aacb16a6e1c584f3
Dustin Langner <dustin.langner1@gmail.com>
2019-04-22 15:23:35 -0700

    Add basic skeleton and UI

--- app/src/main/java/edu/us/ischool/dlangner/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/dlangner/tipcalc/MainActivity.kt
@@ -2,11 +2,18 @@ package edu.us.ischool.dlangner.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val moneyEditText: EditText = findViewById(R.id.moneyText)
+        // moneyEditText.addTextChangedListener(MoneyTextWatcher())
+
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/edu/us/ischool/dlangner/tipcalc/MoneyTextWatcher.kt
@@ -0,0 +1,21 @@
+package edu.us.ischool.dlangner.tipcalc
+
+import android.text.Editable
+import android.text.TextWatcher
+
+class MoneyTextWatcher: TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+
+    }
+}
+
+
